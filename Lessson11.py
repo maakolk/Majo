f58e7ad2cad901458fbc869b3c5c35598b63b5dc
Michael Aakolk <michael.aakolk@gmx.de>
2017-12-26 17:09:44 +0100

    First Commit - Everything green

---
+++ Lessson11.py
@@ -0,0 +1,31 @@
+import serial  # import Serial Library
+import numpy  # Import numpy
+import matplotlib.pyplot as plt  # import matplotlib library
+from drawnow import *
+
+velocity = []
+arduinoData = serial.Serial('com3', 9600)  # Creating our serial object named arduinoData
+plt.ion()  # Tell matplotlib you want interactive mode to plot live data
+cnt = 0
+
+
+def makeFig():  # Create a function that makes our desired plot
+    plt.ylim(0, 50)  # Set y min and max values
+    plt.title('My Live Streaming Sensor Data')  # Plot the title
+    plt.grid(True)  # Turn the grid on
+    plt.ylabel('Windgeschwindigkeit')  # Set ylabels
+    plt.plot(velocity, 'ro-', label='km/h')  # plot the temperature
+    plt.legend(loc='upper left')  # plot the legend
+
+
+while True:  # While loop that loops forever
+    while (arduinoData.inWaiting() == 0):  # Wait here until there is data
+        pass  # do nothing
+    arduinoString = arduinoData.readline()  # read the line of text from the serial port
+    temp = float(arduinoString)  # Convert first element to floating number and put in temp
+    velocity.append(temp)  # Build our tempF array by appending temp readings
+    drawnow(makeFig)  # Call drawnow to update our live graph
+    plt.pause(.000001)  # Pause Briefly. Important to keep drawnow from crashing
+    cnt = cnt + 1
+    if (cnt > 50):  # If you have 50 or more points, delete the first one from the array
+        velocity.pop(0)  # This allows us to just see the last 50 data points
