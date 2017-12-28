fa6c5ed21cd87c98d4c9fc59d41f4a9ec4987cbc
Michael Aakolk <michael.aakolk@gmx.de>
2017-12-28 20:57:07 +0100

    With Figure Size

--- SimulateData.py
+++ SimulateData.py
@@ -5,9 +5,12 @@ import numpy  # Import numpy
 import matplotlib.pyplot as plt  # import matplotlib library
 from drawnow import *
 
+X_LIMIT = 50
+
 
 def makeFig():  # Create a function that makes our desired plot
     plt.ylim(0, 50)  # Set y min and max values
+    plt.xlim(0, X_LIMIT)  # Set x min and max values
     plt.title('Wind Diagramm')  # Plot the title
     plt.grid(True)  # Turn the grid on
     plt.ylabel('Windgeschwindigkeit')  # Set ylabels
@@ -33,22 +36,19 @@ class ArduinoConnection:
         self.count += 1
         return ((math.sin(2 * math.pi * self.count / self.SINUS_LENGTH) + 1) + random.random() * 0.5) * 20
 
-velocityArray = []
 plt.ion()  # Tell matplotlib you want interactive mode to plot live data
-cnt = 0
+plt.figure(figsize=(18, 6), dpi=80)
 
 connection = ArduinoConnection()
 
+velocityArray = []
 while True:  # While loop that loops forever
-    # arduinoData = connection.getRealData()
-    arduinoData = connection.getSimulateData()
-    velocity = float(arduinoData)  # Convert first element to floating number and put in temp
+    velocity = connection.getSimulateData()
     velocityArray.append(velocity)  # Build our tempF array by appending temp readings
+    if (len(velocityArray) > X_LIMIT + 1 ):  # If you have 50 or more points, delete the first one from the array
+        velocityArray.pop(0)  # This allows us to just see the last 50 data points
     drawnow(makeFig)  # Call drawnow to update our live graph
     plt.pause(.000001)  # Pause Briefly. Important to keep drawnow from crashing
-    cnt = cnt + 1
-    if (cnt > 50):  # If you have 50 or more points, delete the first one from the array
-        velocityArray.pop(0)  # This allows us to just see the last 50 data points
 
 
 
