7795bf16289c4eed5791c57263161825405455ea
Michael Aakolk <michael.aakolk@gmx.de>
2017-12-28 22:30:44 +0100

    Improved graphic output handling

--- SimulateData.py
+++ SimulateData.py
@@ -4,18 +4,9 @@ import serial  # import Serial Library
 import numpy  # Import numpy
 import matplotlib.pyplot as plt  # import matplotlib library
 from drawnow import *
+from matplotlib.animation import FuncAnimation
 
-X_LIMIT = 50
-
-
-def makeFig():  # Create a function that makes our desired plot
-    plt.ylim(0, 50)  # Set y min and max values
-    plt.xlim(0, X_LIMIT)  # Set x min and max values
-    plt.title('Wind Diagramm')  # Plot the title
-    plt.grid(True)  # Turn the grid on
-    plt.ylabel('Windgeschwindigkeit')  # Set ylabels
-    plt.plot(velocityArray, 'ro-', label='km/h')  # plot the temperature
-    plt.legend(loc='upper left')  # plot the legend
+X_LIMIT = 100
 
 class ArduinoConnection:
     SINUS_LENGTH = 50
@@ -36,20 +27,26 @@ class ArduinoConnection:
         self.count += 1
         return ((math.sin(2 * math.pi * self.count / self.SINUS_LENGTH) + 1) + random.random() * 0.5) * 20
 
-plt.ion()  # Tell matplotlib you want interactive mode to plot live data
-plt.figure(figsize=(18, 6), dpi=80)
-
-connection = ArduinoConnection()
 
 velocityArray = []
-while True:  # While loop that loops forever
+connection = ArduinoConnection()
+
+
+def update(frame):
     velocity = connection.getSimulateData()
     velocityArray.append(velocity)  # Build our tempF array by appending temp readings
-    if (len(velocityArray) > X_LIMIT + 1 ):  # If you have 50 or more points, delete the first one from the array
+    if (len(velocityArray) > X_LIMIT + 1):  # If you have 50 or more points, delete the first one from the array
         velocityArray.pop(0)  # This allows us to just see the last 50 data points
-    drawnow(makeFig)  # Call drawnow to update our live graph
-    plt.pause(.000001)  # Pause Briefly. Important to keep drawnow from crashing
+    ax.clear()
+    ax.grid(True)  # Turn the grid on
+    ax.set_ylabel('Windgeschwindigkeit')  # Set ylabels
+    ax.set_ylim(0, 50)
+    ax.set_xlim(0, X_LIMIT)
+    ax.plot(velocityArray, 'ro-', label='km/h')
+    ax.legend(loc='upper left')  # plot the legend
 
-
-
-
+fig = plt.figure(figsize=(23, 10), dpi=80)
+ax = fig.add_subplot(111)
+animation = FuncAnimation(fig, update, interval=1, frames=10)
+plt.show()
+# animation.save('rain.gif', writer='imagemagick', fps=30, dpi=40)
