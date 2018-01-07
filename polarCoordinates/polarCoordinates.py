d5ee93989b2afef9b83742f96d0e087bbc9294ff
Michael Aakolk <michael.aakolk@gmx.de>
2018-01-07 18:18:27 +0100

    Radian

--- polarCoordinates/polarCoordinates.py
+++ polarCoordinates/polarCoordinates.py
@@ -10,17 +10,24 @@ import matplotlib.pyplot as plt
 # theta = 2 * np.pi * r
 
 alpha = 30.0
+alphaRad = 2 * np.pi * alpha / 360.
+
 r = [0.0001, 2.0]
-theta = [ 2 * np.pi * alpha / 360., 2 * np.pi * alpha / 360.]
+theta = [alphaRad, alphaRad]
 
 ax = plt.subplot(111, projection='polar')
 ax.plot(theta, r, color = "red")
 ax.set_rmax(2)
 ax.set_rticks([])  # less radial ticks
 # ax.set_xticks(np.pi/180. * np.linspace(0,  360, 16, endpoint=False))
-ax.set_xticklabels(['E', '', 'N', '', 'W', '', 'S', ''])
-ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
+# ax.set_xticklabels(['E', '', 'N', '', 'W', '', 'S', ''])
+
+
+xT=plt.xticks()[0]
+xL=['0',r'$\frac{\pi}{4}$',r'$\frac{\pi}{2}$',r'$\frac{3\pi}{4}$',\
+    r'$\pi$',r'$\frac{5\pi}{4}$',r'$\frac{3\pi}{2}$',r'$\frac{7\pi}{4}$']
+plt.xticks(xT, xL)
+
 ax.grid(True)
 
-ax.set_title("A line plot on a polar axis", va='bottom')
 plt.show()
