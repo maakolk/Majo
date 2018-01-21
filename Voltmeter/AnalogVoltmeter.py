88ed890e54dd9c7d631233b448cac4de89127ca6
Michael Aakolk <michael.aakolk@gmx.de>
2018-01-21 22:53:18 +0100

    Increase Baud Rate

--- Voltmeter/AnalogVoltmeter.py
+++ Voltmeter/AnalogVoltmeter.py
@@ -8,8 +8,8 @@ from matplotlib.animation import FuncAnimation
 
 
 # https://stackoverflow.com/questions/5285912/how-can-i-create-a-frontend-for-matplotlib
-REFRESH_MS = 1000
-X_LIMIT_MS = 4000
+REFRESH_MS = 500
+X_LIMIT_MS = 1000
 X_TICKS_COUNT = 20
 
 VOLTAGE_SHOW = True
@@ -35,7 +35,7 @@ class ArduinoConnection:
     def getRealData(self):
         first = False
         if (self.arduinoConnection == None):
-            self.arduinoConnection = serial.Serial('com4', 9600)  # Creating our serial object named arduinoData
+            self.arduinoConnection = serial.Serial('com4', 115200)  # Creating our serial object named arduinoData
             first = True
         while (self.arduinoConnection.inWaiting() == 0):  # Wait here until there is data
             pass  # do nothing
--- Voltmeter/analogReadSerialWriteJoyStickAndLedSinus/analogReadSerialWriteJoyStickAndLedSinus.ino
+++ Voltmeter/analogReadSerialWriteJoyStickAndLedSinus/analogReadSerialWriteJoyStickAndLedSinus.ino
@@ -11,7 +11,7 @@ const int ledPin =  3;// the number of the LED pin
 
 void setup() {
   // start serial port at 9600 bps:
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // set the digital pin as output:
   pinMode(ledPin, OUTPUT);
@@ -21,16 +21,16 @@ void setup() {
 void loop() {
  sensorValue = analogRead(A0);
  Serial.println(sensorValue);
-
- //>>>>>>>>>>>>>>>>>>>>>>>
-    sensorValue2 = analogRead(A1);
-    frequency = map(sensorValue2, 0,1023, 1, 100);
-
- //>>>>>>>>>>>>>>>>>>>>>>>>
-    current = millis();
-    if(current - start > 1 / frequency){
-       start = start + 1 / frequency;
-    }
-    ledValue = ( sin( 2 * PI * millis() / 1000.0 * frequency) + 1) * 10;
-    analogWrite(ledPin, ledValue);
+//
+// //>>>>>>>>>>>>>>>>>>>>>>>
+//    sensorValue2 = analogRead(A1);
+//    frequency = map(sensorValue2, 0,1023, 1, 100);
+//
+// //>>>>>>>>>>>>>>>>>>>>>>>>
+//    current = millis();
+//    if(current - start > 1 / frequency){
+//       start = start + 1 / frequency;
+//    }
+//    ledValue = ( sin( 2 * PI * millis() / 1000.0 * frequency) + 1) * 10;
+//    analogWrite(ledPin, ledValue);
 }
